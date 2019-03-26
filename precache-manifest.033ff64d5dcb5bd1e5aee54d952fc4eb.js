7b06fed317b7794ed518cb218899703c485bd9bd
Jackie Lealess <jlealess@gmail.com>
2019-03-26 09:42:37 -0400

    Updates

---
+++ precache-manifest.033ff64d5dcb5bd1e5aee54d952fc4eb.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = [
+  {
+    "revision": "c720d4facd38792e18ac",
+    "url": "/rangle-type-scale/static/css/main.2c81766e.chunk.css"
+  },
+  {
+    "revision": "c720d4facd38792e18ac",
+    "url": "/rangle-type-scale/static/js/main.5fea1468.chunk.js"
+  },
+  {
+    "revision": "4f0c98cf9b89cc3f29d1",
+    "url": "/rangle-type-scale/static/js/runtime~main.d4d52137.js"
+  },
+  {
+    "revision": "7d39ba448e2458e4eca6",
+    "url": "/rangle-type-scale/static/js/2.e41f8e7e.chunk.js"
+  },
+  {
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Regular-908f9be9038dcb496f9e4cbc0b20a04f.908f9be9.woff2"
+  },
+  {
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Light-a1f457c86a8442c72cdf16261d825731.a1f457c8.woff2"
+  },
+  {
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Regular-c610cb5ea5d76df2ae998049006c3e54.c610cb5e.woff"
+  },
+  {
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Regular-6ea5b90e309003efc2f6e84cca89be9d.6ea5b90e.eot"
+  },
+  {
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Light-94ee2a9917d824f830593659433bb9a9.94ee2a99.eot"
+  },
+  {
+    "revision": "fdbb30c154ebbe6a2d166152e9263931",
+    "url": "/rangle-type-scale/index.html"
+  }
+];
