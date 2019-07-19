240258c7d2f9d67dc117f8927c8b22f12a22aef8
Evan Schultz <evan@rangle.io>
2019-07-19 11:34:27 -0400

    Updates

---
+++ precache-manifest.c9cd9dcda23c9fd294a76298844e9dc3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8df239660c7af9d636",
+    "url": "/rangle-type-scale/static/css/main.2fa61800.chunk.css"
+  },
+  {
+    "revision": "9c8df239660c7af9d636",
+    "url": "/rangle-type-scale/static/js/main.87985ab0.chunk.js"
+  },
+  {
+    "revision": "4f0c98cf9b89cc3f29d1",
+    "url": "/rangle-type-scale/static/js/runtime~main.d4d52137.js"
+  },
+  {
+    "revision": "8ffebfd6edeb1185fdb9",
+    "url": "/rangle-type-scale/static/js/2.99442665.chunk.js"
+  },
+  {
+    "revision": "7c7d464e9a595e4a2f085b3852c21da4",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Medium.7c7d464e.woff2"
+  },
+  {
+    "revision": "ff84248055a4b96ea40572ad4b71b756",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Bold.ff842480.woff2"
+  },
+  {
+    "revision": "15e32ab996d3c55cc6c5e06e65d5afee",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Heavy.15e32ab9.woff2"
+  },
+  {
+    "revision": "dab55fbd3a94031e90a127f9170f4f48",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Bold.dab55fbd.woff"
+  },
+  {
+    "revision": "740ac45d450123b5df43b8cade5890fd",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Medium.740ac45d.woff"
+  },
+  {
+    "revision": "51da3b8d7a7a2a7b896a63836b8ac7e0",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Heavy.51da3b8d.woff"
+  },
+  {
+    "revision": "a1f457c86a8442c72cdf16261d825731",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Light.a1f457c8.woff2"
+  },
+  {
+    "revision": "908f9be9038dcb496f9e4cbc0b20a04f",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Regular.908f9be9.woff2"
+  },
+  {
+    "revision": "c610cb5ea5d76df2ae998049006c3e54",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Regular.c610cb5e.woff"
+  },
+  {
+    "revision": "61961b16973e55442e8b086bf73d1264",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Medium.61961b16.eot"
+  },
+  {
+    "revision": "4e12a42fea3d43377cc9cfa36ee0ac46",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Bold.4e12a42f.eot"
+  },
+  {
+    "revision": "61fc60667cf7ca9c25b0a34e9a967c42",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Heavy.61fc6066.eot"
+  },
+  {
+    "revision": "6ea5b90e309003efc2f6e84cca89be9d",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Regular.6ea5b90e.eot"
+  },
+  {
+    "revision": "94ee2a9917d824f830593659433bb9a9",
+    "url": "/rangle-type-scale/static/media/RangleRiformaWeb-Light.94ee2a99.eot"
+  },
+  {
+    "revision": "fb5eae44304e21fbe4a299a0a40e9349",
+    "url": "/rangle-type-scale/static/media/modular-scale-logo.fb5eae44.svg"
+  },
+  {
+    "revision": "d644f1f333ab192cdee49bf4b14752c4",
+    "url": "/rangle-type-scale/index.html"
+  }
+];
--- src/utilities/Colors.js
+++
@@ -1,6 +0,0 @@
-export const Colors = {
-  black: "#262626",
-  darkGray: "#737373",
-  lightGray: "#b5b5b5",
-  white: "#ffffff"
-}
